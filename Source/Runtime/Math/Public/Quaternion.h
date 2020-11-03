80da345d718e1f0f7e14cf60eac0dff57912c9a9
ideugu <guru@dustinlee.me>
2020-11-03 11:34:52 +0900

    로컬 변수 이름 변경

--- Source/Runtime/Math/Public/Quaternion.h
+++ Source/Runtime/Math/Public/Quaternion.h
@@ -114,23 +114,23 @@ FORCEINLINE void Quaternion::FromMatrix(const Matrix3x3& InMatrix)
 
 FORCEINLINE void Quaternion::FromVector(const Vector3& InVector, const Vector3& InUp)
 {
-	Vector3 viewX, viewY, viewZ;
+	Vector3 localX, localY, localZ;
 
 	// 단위 Z축 생성
-	viewZ = InVector.Normalize();
+	localZ = InVector.Normalize();
 	// 시선 방향과 월드 Y축이 평행한 경우
-	if (Math::Abs(viewZ.Y) >= (1.f - SMALL_NUMBER))
+	if (Math::Abs(localZ.Y) >= (1.f - SMALL_NUMBER))
 	{
 		// 특이 상황에서 로컬 X 좌표 값을 임의로 지정.
-		viewX = Vector3::UnitX;
+		localX = Vector3::UnitX;
 	}
 	else
 	{
-		viewX = InUp.Cross(viewZ).Normalize();
+		localX = InUp.Cross(localZ).Normalize();
 	}
-	viewY = viewZ.Cross(viewX).Normalize();
+	localY = localZ.Cross(localX).Normalize();
 
-	FromMatrix(Matrix3x3(viewX, viewY, viewZ));
+	FromMatrix(Matrix3x3(localX, localY, localZ));
 }
 
 FORCEINLINE constexpr void Quaternion::FromAxisAngle(const Vector3& InAxis, float InAngleDegree)
